98d75c910226e81c2ea1376201b9abd90e3aac14
Nkanga Pedro <117827721+Nkanga-Pedro@users.noreply.github.com>
2023-02-07 00:01:01 +0000

    Add files via upload

---
+++ Classif_Instancia_Viz_prox.R
@@ -0,0 +1,8 @@
+head(iris)
+amostra=sample(2,150,replace = T,prob = c(0.7,0.3))
+iristreino=iris[amostra==1,]
+classificar=iris[amostra==2,]
+dim(iristreino)
+dim(iristeste)
+previsao=knn(iristreino[,1:4],classificar[,1:4],iristreino[,5],k=3)
+table(classificar[,5],previsao)
